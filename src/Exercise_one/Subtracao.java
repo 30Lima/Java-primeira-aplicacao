f442855e8cdafcde942f0f1a531046c91b3ec2c9
30lima <pedrolimaa354@gmail.com>
2025-03-01 14:01:56 -0300

    Creating class "Subtracao" for subtract products.

--- src/Exercise_one/Subtracao.java
+++ src/Exercise_one/Subtracao.java
@@ -6,7 +6,6 @@ public class Subtracao {
          * Crie uma classe Subtracao para imprimir o resultado de 10 - 5 no console.
          *
          **/
-
         int subtracao = 10 - 5;
         System.out.println(subtracao);
     }
